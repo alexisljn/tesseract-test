d9ffd60993a5c0d37f22cbdcc1469c117e10a6c0
alexisljn <alexis.lejeune@gmx.fr>
2021-08-15 20:43:27 +0200

    No web

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const app = express();
 const Tesseract = require("tesseract.js");
+const fs = require('fs');
 
-app.set('view engine', 'ejs');
+(async () => {
+    const directoryContent = fs.readdirSync('./src');
+    // console.log("directoryContent", directoryContent);
 
-// app.use(express.json({limit: "50mb"}));
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 1000000}))
-app.get('/', (req, res) => {
-    res.render('index');
-})
+    for (const file of directoryContent) {
+        const result = await Tesseract.recognize(`./src/${file}`, 'fra', {logger: m => console.log(m)});
+        fs.appendFile('questions.txt', result.data.text, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        })
+    }
 
-app.post('/detect', async (req, res) => {
-    const data = await Tesseract.recognize('Toto-logo.jpg', 'fra', {logger: m => console.log(m)});
-    console.log(data);
-})
+})();
 
-app.listen(3000, () => {
-    console.log('server launched');
-});
+
+
